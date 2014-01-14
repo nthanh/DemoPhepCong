d2b642d634980c968b2aa08b06b83bc98efdc482
tuyento <tuyento12291@gmail.com>
2014-01-14 10:58:41 +0700

    Sum
    Tinh tong

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
